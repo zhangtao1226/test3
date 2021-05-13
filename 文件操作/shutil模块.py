05d06ebd13f386dc87a0425e0fa1b5f101dc8602
zhangtao <zhangtao1226@outlook.com>
2021-05-13 11:45:15 +0800

    Create shutil模块.py

---
+++ 文件操作/shutil模块.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/3 12:16
+# @Author  : zhangtao
+# @File    : shutil模块.py
+# @Software: PyCharm
+
+import os
+import shutil
+
+# os.mkdir(r'./test_mkdir')
+shutil.move(r'./test.py', r'./test_mkdir')
