c0adcf5796949d65ad7668ddff3e452e4978cedb
zhangtao <zhangtao1226@outlook.com>
2021-05-13 11:45:17 +0800

    Create test.py

---
+++ 文件操作/test_mkdir/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/2 17:46
+# @Author  : zhangtao
+# @File    : test.py
+# @Software: PyCharm
+
+import DirAndFileClass as dir
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
