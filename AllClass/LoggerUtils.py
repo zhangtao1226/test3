2c62fa6b1c2c914fad0f6ec1a055650714eb5c32
zhangtao <zhangtao1226@outlook.com>
2021-12-08 10:18:20 +0800

    Create LoggerUtils.py
    test

---
+++ AllClass/LoggerUtils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/31 15:48
+# @Author  : zhangtao
+# @File    : LoggerUtils.py
+# @Software: PyCharm
+
+import os
+import sys
+import time
+import logging
+import logging.handlers
+
+class LoggerUtils:
+    def __init__(self, set_level="INFO", name=os.path.split()):
+        pass
+
+
+
